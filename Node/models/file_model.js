15544dd556ae541f89a789b62b3c91be2239a6d9
miffi <miffi@protonmail.com>
2018-02-17 18:50:10 -0500

    removed shitty sql library

--- Node/models/file_model.js
+++ Node/models/file_model.js
@@ -1,5 +1,5 @@
-var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('db.db');
+var Database = require('better-sqlite3');
+var db = new Database('db.db');
 
 const file_model = {
     db_id: null,
@@ -13,27 +13,11 @@ const file_model = {
     },
 
     load_by_id: function(id) {
-        db.serialize(function() {
-            db.all(`SELECT * FROM files WHERE id=${id}`, function(err, rows) {
-                rows.forEach(function(row) {
-                    console.log(row);
-                    this.db_id = row.id;
-                    this.name = row.name;
-                    this.data = row.data;
-                })
-            });
-        });
+        let q = `SELECT * FROM files WHERE id=${id}`;
     },
 
     write_new_file: function() { 
-        //console.log(this.name);
-        db.serialize(function() {
-            db.run("INSERT INTO files (id, name, data) VALUES ($id, $name, $data)", {
-                $id: this.db_id,
-                $name: this.name,
-                $data: this.data
-            });
-        });
+        db.run("INSERT INTO files (id, name, data) VALUES ($id, $name, $data)");
     },
 
     print: function() {
@@ -43,27 +27,21 @@ const file_model = {
 
     //Below here is testing code
     create_test_table: function() {
-        db.serialize(function() {
-            db.run("DROP TABLE files");
-            db.run("CREATE TABLE files (id INTEGER, name TEXT, data BLOB)");
-        });
+        db.run("DROP TABLE files");
+        db.run("CREATE TABLE files (id INTEGER, name TEXT, data BLOB)");
         console.log("table create");
     }
 }
 
 
 const sanity_test = function() {
+
     const test_model = Object.create(file_model);
-    test_model.create_test_table();
 
     test_model.set(1, "gay", "0101010101010");
     test_model.print();
     test_model.write_new_file()
-    
-    const new_model = Object.create(file_model);
-    new_model.load_by_id(1);
-    new_model.print();
-    
+            
 }
 
 sanity_test();
