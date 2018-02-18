2aee5c597fed904b5dd5e34a512e407a73c4f62e
miffi <miffi@protonmail.com>
2018-02-17 19:34:46 -0500

    Model actually working with sqlite3

--- Node/models/file_model.js
+++ Node/models/file_model.js
@@ -2,27 +2,36 @@ var Database = require('better-sqlite3');
 var db = new Database('db.db');
 
 const file_model = {
-    db_id: null,
+    id: null,
     name: null,
     data: null,
 
     set: function(id, name, data) {
-        this.db_id = id;
+        this.id = id;
         this.name = name;
         this.data = data;
     },
 
     load_by_id: function(id) {
         let q = `SELECT * FROM files WHERE id=${id}`;
+        let row = db.prepare(q).get();
+        console.log(row.id);
+        this.id = row.id;
+        this.name = row.name;
+        this.data = data;
+
     },
 
     write_new_file: function() { 
-
-        db.prepare("INSERT INTO files (id, name, data) VALUES ($id, $name, $data)");
+        id = this.id;
+        name = this.name;
+        data = this.data;
+        var query = `INSERT INTO files (id, name, data) VALUES ('${id}', '${name}', '${data}')`;
+        db.prepare(query).run();
     },
 
     print: function() {
-        console.log("id: " + this.db_id, this.name, this.data)
+        console.log("id: " + this.id, this.name, this.data)
     },
         
 
@@ -42,6 +51,10 @@ const sanity_test = function() {
     test_model.set(1, "gay", "0101010101010");
     test_model.print();
     test_model.write_new_file()
+
+    const new_model = Object.create(file_model);
+    new_model.load_by_id(1);
+    new_model.print();
             
 }
 
