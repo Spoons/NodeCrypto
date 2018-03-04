5b6a92ebbd1fb411e79e6e3daec7124a8ea3cab2
miffi <michaelciociola123@gmail.com>
2018-03-04 14:39:40 -0500

    Fixed styling

--- models/file_model.js
+++ models/file_model.js
@@ -3,86 +3,85 @@ let db = require("./model_generic").db;
 let schema_p = require("./schema");
 let file_model = function() {
 
-  this.schema = {
-    properties: {
-      table_name: "files",
-      exported: false,
-    },
-    id: {
-      db_type: "INTEGER PRIMARY KEY",
-      column_name: "id",
-      value: null,
-    },
-    name: {
-      db_type: "TEXT",
-      column_name: "name",
-      value: null,
-    },
-    data: {
-      db_type: "BLOB",
-      column_name: "data",
-      value: null,
-    },
-    user: {
-      db_type: "INTEGER",
-      db_foreign_table: "users",
-      db_foreign_column: "id",
-      column_name: "user",
-      value: null,
+    this.schema = {
+        properties: {
+            table_name: "files",
+            exported: false,
+        },
+        id: {
+            db_type: "INTEGER PRIMARY KEY",
+            column_name: "id",
+            value: null,
+        },
+        name: {
+            db_type: "TEXT",
+            column_name: "name",
+            value: null,
+        },
+        data: {
+            db_type: "BLOB",
+            column_name: "data",
+            value: null,
+        },
+        user: {
+            db_type: "INTEGER",
+            db_foreign_table: "users",
+            db_foreign_column: "id",
+            column_name: "user",
+            value: null,
 
+        },
+        key: {
+            db_type: "INTEGER",
+            db_foreign_table: "keys",
+            db_foreign_column: "id",
+            column_name: "key",
+            value: null,
+        }
     },
-    key: {
-      db_type: "INTEGER",
-      db_foreign_table: "keys",
-      db_foreign_column: "id",
-      column_name: "key",
-      value: null,
+
+    this.schema = Object.assign(this.schema, schema_p);
+
+    this.set = function(id, name, data, user, key) {
+        this.schema.id.value = id;
+        this.schema.name.value = name;
+        this.schema.data.value = data;
+        this.schema.user.value = user;
+        this.schema.key.value = key;
+        this.schema.write();
+    },
+    this.to_string = function() {
+        let prop = this.schema.get_schema_properties();
+        let output = "";
+        prop.forEach(function(p) {
+            output+= p.column_name + ": " + p.value + " ";
+        });
+        return(output);
     }
-  },
-
-  this.schema = Object.assign(this.schema, schema_p);
-  console.log(this.schema);
-
-  this.set = function(id, name, data, user, key) {
-    this.schema.id.value = id;
-    this.schema.name.value = name;
-    this.schema.data.value = data;
-    this.schema.user.value = user;
-    this.schema.key.value = key;
-    this.schema.write();
-  },
-  this.to_string = function() {
-    let prop = this.schema.get_schema_properties();
-    let output = "";
-    prop.forEach(function(p) {
-      output+= p.column_name + ": " + p.value + " ";
-    });
-    return(output);
-  }
-  this.print = function() {
-    console.log(this.to_string());
-  }
+    this.print = function() {
+        console.log(this.to_string());
+    }
 }
 file_model.sanity = function() {
-  const test_model = new file_model();
-  test_model.schema.create_table();
+    const test_model = new file_model();
+    test_model.schema.create_table();
 
-  test_model.set("1", "cats", "01010101010", "1", "1");
-  let t1 = test_model.to_string();
-  test_model.print();
+    test_model.set("1", "cats", "01010101010", "1", "1");
+    let t1 = test_model.to_string();
+    test_model.print();
 
-  const new_model = new file_model();
+    const new_model = new file_model();
 
-  new_model.schema.load(1);
-  let t2 = new_model.to_string();
-  new_model.print();
+    new_model.schema.load(1);
+    let t2 = new_model.to_string();
+    new_model.print();
 
-  if (t1 === t2) {
-    console.log("file-model: sanity check pass!");
-    return (true);
-  } else {
-    return (false);
-  }
+    if (t1 === t2) {
+        console.log("file-model: sanity check pass!");
+        return (true);
+    } else {
+        return (false);
+    }
 }
 
 file_model.sanity();
--- models/schema.js
+++ models/schema.js
@@ -1,64 +1,65 @@
 let db = require("./model_generic").db;
 const schema = {
-  create_table: function() {
-    let properties = this.get_schema_properties();
+    create_table: function() {
+        let properties = this.get_schema_properties();
 
-    db.prepare("DROP TABLE IF EXISTS files").run();
-    let table_create_query = `CREATE TABLE ${this.get_table_name()} (`;
-      properties.forEach(function(prop){
-        table_create_query += prop.column_name +" "+ prop.db_type + ", ";
+        db.prepare("DROP TABLE IF EXISTS files").run();
+        let table_create_query = `CREATE TABLE ${this.get_table_name()} (`;
+        properties.forEach(function(prop){
+            table_create_query += prop.column_name +" "+ prop.db_type + ", ";
 
-      });
-      table_create_query = table_create_query.substring(0, table_create_query.length - 2);
-      table_create_query += ');';
-      //console.log(table_create_query);
-      db.prepare(table_create_query).run();
+        });
+        table_create_query = table_create_query.substring(0, table_create_query.length - 2);
+        table_create_query += ');';
+        //console.log(table_create_query);
+        db.prepare(table_create_query).run();
     },
+
     drop_table: function() {
-      db.prepare("DROP TABLE IF EXISTS files").run();
+        db.prepare("DROP TABLE IF EXISTS files").run();
     },
 
     write: function() {
-      let properties = this.get_schema_properties();
+        let properties = this.get_schema_properties();
 
-      //console.log("writing object to db");
-      let query = "INSERT OR REPLACE INTO " + this.properties.table_name + "(";
-      let values = "";
-      properties.forEach(function(p) {
-        query+=p.column_name + ", ";
-        values+="\'"+p.value + "\', ";
-      });
-      query = query.substring(0, query.length-2) + ") VALUES (";
-      query += values + ")";
-      query = query.substring(0, query.length-3);
-      query += ");"
-      db.prepare(query).run();
+        //console.log("writing object to db");
+        let query = "INSERT OR REPLACE INTO " + this.properties.table_name + "(";
+        let values = "";
+        properties.forEach(function(p) {
+            query+=p.column_name + ", ";
+            values+="\'"+p.value + "\', ";
+        });
+        query = query.substring(0, query.length-2) + ") VALUES (";
+        query += values + ")";
+        query = query.substring(0, query.length-3);
+        query += ");"
+        db.prepare(query).run();
     },
 
     load: function(id) {
-      let q = `SELECT * FROM ${this.properties.table_name} WHERE id=${id};`;
-      let v = db.prepare(q).get();
-      let properties = this.get_schema_properties();
+        let q = `SELECT * FROM ${this.properties.table_name} WHERE id=${id};`;
+        let v = db.prepare(q).get();
+        let properties = this.get_schema_properties();
 
-      let object = this;
-      properties.forEach(function(p) {
-        object[p.column_name].value = v[p.column_name];
-      });
+        let object = this;
+        properties.forEach(function(p) {
+            object[p.column_name].value = v[p.column_name];
+        });
     },
 
     get_schema_properties: function() {
-      let list = [];
-      for (var property in this) {
-        if (property.exported != false
-          && typeof(this[property]) !== "function"
-          && property !== "properties") {
-            list.push(this[property]);
-          }
-        }
-        return (list);
-      },
-      get_table_name: function() {
+        let list = [];
+        for (var property in this) {
+            if (property.exported != false
+                && typeof(this[property]) !== "function"
+                && property !== "properties") {
+                    list.push(this[property]);
+                }
+            }
+            return (list);
+        },
+    get_table_name: function() {
         return (this.properties.table_name);
-      }
+    }
 }
 module.exports = schema;
