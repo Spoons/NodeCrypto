3c6d268c235dc5a09ae6ceaf59938e4266b33960
miffi <miffi@protonmail.com>
2018-03-13 17:26:58 -0400

    Set up skeleton for file controller.

--- controllers/file-controller.js
+++ controllers/file-controller.js
@@ -1,3 +1,5 @@
+const FileModel = require('../models/file_model');
+
 exports.file_route_get = (req,res) => {
     res.render('files/index');
 }
@@ -5,7 +7,7 @@ exports.file_route_get = (req,res) => {
 exports.file_id_get = (req,res) => {
     const fileId = req.params.id;
     if (fileId){
-        res.render('files/file', {fileId: fileId});    
+        res.render('files/file', {fileId: fileId});
     }else{
         res.render('files/file');
     }
@@ -14,11 +16,38 @@ exports.file_id_get = (req,res) => {
 exports.file_transfer_get = (req,res) => {
     const fileId = req.params.id;
     if (fileId){
-        res.render('files/transfer', {fileId: fileId});    
+        res.render('files/transfer', {fileId: fileId});
     }else{
         res.render('files/transfer');
     }
 }
 
 let file_controller = {
+  create_file: function(file_name, file_data, owner){
+    //TODO: create file functionality
+  },
+
+  get_file_properties: function(file_id){
+    //TODO: get file data with ID [JSON]
+  },
+
+  get_file_binary: function(file_id){
+    //TODO: get file data with ID [DATA]
+  },
+
+  update_file_data: function(file_id, file_data){
+    //TODO: update file with ID with data
+  },
+
+  delete_file: function(file_id){
+    //TODO: delete file functionality
+  },
+
+  list_files_by_user: function(user_id){
+    //TODO: get all files by given user ID
+  },
+
+  list_all_files_ADMIN: function(){
+
+  }
 }
