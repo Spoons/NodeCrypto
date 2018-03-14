0c0ecc37a129fe4668c7bd584d4fb87112eb8993
Thomas Nix <tnix2@uncc.edu>
2018-03-14 00:44:17 -0400

    Set up partial loading and gulp assets directories.

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/dist'));
 app.use(fileUpload());
 app.set('view engine','ejs');
 
